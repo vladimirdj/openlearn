ae2156cbe0a298d371931ba04b353189823d0215
Subhadeep Dey <contact.sdey@gmail.com>
2017-10-22 22:39:25 +0530

    Removed Instagram Gallery.

--- index.php
+++ index.php
@@ -485,26 +485,6 @@
 		</div>
 	</div>
 
-	<div id="fh5co-gallery" class="fh5co-bg-section">
-		<div class="row text-center">
-			<h2><span>Instagram Gallery</span></h2>
-		</div>
-		<div class="row">
-			<div class="col-md-3 col-padded">
-				<a href="#" class="gallery" style="background-image: url(images/project-5.jpg);"></a>
-			</div>
-			<div class="col-md-3 col-padded">
-				<a href="#" class="gallery" style="background-image: url(images/project-2.jpg);"></a>
-			</div>
-			<div class="col-md-3 col-padded">
-				<a href="#" class="gallery" style="background-image: url(images/project-3.jpg);"></a>
-			</div>
-			<div class="col-md-3 col-padded">
-				<a href="#" class="gallery" style="background-image: url(images/project-4.jpg);"></a>
-			</div>
-		</div>
-	</div>
-
 	<footer id="fh5co-footer" role="contentinfo" style="background-image: url(images/mountain.jpg);">
 		<div class="overlay"></div>
 		<div class="container">
