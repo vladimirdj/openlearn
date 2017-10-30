7826d11308d9ed7775b952b4f9d9dce57665a0ac
Subhadeep Dey <contact.sdey@gmail.com>
2017-10-30 10:40:57 +0530

    Improved Error Messages in config.php

--- PHP/config.php
+++ PHP/config.php
@@ -1,16 +1,13 @@
 <?php
-   define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', 'toor');
-   define('DB_DATABASE', 'openlearn');
-   $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+    define('DB_SERVER', 'localhost');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', 'toor');
+    define('DB_DATABASE', 'openlearn');
+   
+    $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
-   if($db)
-   {
-       echo "Connection successful!";
-   }
-   else
-   {
-       echo "Connection failed!";
-   }
+    if(!$db)
+    {
+        die ("Error " . mysqli_connect_errno() . ": " . mysqli_connect_error(). $PHP_EOL);
+    }
 ?>
