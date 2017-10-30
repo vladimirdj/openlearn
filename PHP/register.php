bc82ccf864bfb18915e9343e1461f19ee39e9681
Subhadeep Dey <contact.sdey@gmail.com>
2017-10-31 01:17:23 +0530

    Modified register.php

--- PHP/register.php
+++ PHP/register.php
@@ -1,3 +1,195 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<link rel="shortcut icon" href="favicon.png" />
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link rel="stylesheet" href="css/font-awesome-4.7.0/css/font-awesome.min.css">
+	<title>Become an Instructor &mdash; OpenLearn</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Website Template by freehtml5.co" />
+	<meta name="keywords" content="free website templates, free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
+	<meta name="author" content="freehtml5.co" />
+
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Roboto+Slab:300,400" rel="stylesheet">
+
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<!-- Owl Carousel  -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="css/flexslider.css">
+
+	<!-- Pricing -->
+	<link rel="stylesheet" href="css/pricing.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	<style>
+			.modal {
+			text-align: center;
+			padding: 0!important;
+			}
+
+			.modal:before {
+			content: '';
+			display: inline-block;
+			height: 100%;
+			vertical-align: middle;
+			margin-right: -4px;
+			}
+
+			.modal-dialog {
+			display: inline-block;
+			text-align: left;
+			vertical-align: middle;
+			}
+		</style>
+
+	</head>
+	<body>
+
+	<div class="fh5co-loader"></div>
+
+	<div id="page">
+	<nav class="fh5co-nav" role="navigation">
+		<div class="top">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12 text-right">
+						<p class="site">www.openlearn.com</p>
+						<p class="num">Call: +01 123 456 7890</p>
+						<ul class="fh5co-social">
+							<li><a href="#"><i class="icon-facebook2"></i></a></li>
+							<li><a href="#"><i class="icon-twitter2"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble2"></i></a></li>
+							<li><a href="#"><i class="icon-github"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="top-menu">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-2">
+					<div id="fh5co-logo"><a href="index.php"><i class="icon-study"></i><span>&nbsp;Open</span><font color="#2D6CDF">Learn</font></a></div>
+				</div>
+				<div class="col-xs-10 text-right menu-1">
+					<ul>
+						<li><a href="index.php">Home</a></li>
+						<li><a href="courses.php">Courses</a></li>
+						<li><a href="instructors.php">Instructors</a></li>
+						<li><a href="#"  data-toggle="modal" data-target="#livestream">Livestream</a></li>
+						<li><a href="about.php">About</a></li>
+						<li><a href="contact.php">Contact</a></li>
+						<li class="btn-cta" data-toggle="modal" data-target="#myModal"><a href="#"><span>Login</span></a></li>
+						<li class="btn-cta"><a href="signup.php"><span>Become an Instructor</span></a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	</nav>
+
+	<!-- Modal - For Livestream-->
+	<div class="modal fade" id="livestream" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					<h4 class="modal-title" id="Livestream_modal">Join Livestream</h4>
+				</div>
+
+				<div class="modal-body">
+					<form action="livestream.php" method="get">
+						<div class="form-group">
+								<label for="Code_Livestream">Enter the invite code of the Livestream you want to join</label>
+								<input type="text" class="form-control" id="livestream_link" aria-describedby="emailHelp" placeholder="Livestream Invite Code" required>
+							</div>
+				</div>
+
+				<div class="modal-footer">
+						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+						<input type="submit" value="Join Livestream" class="btn btn-primary">
+				</form>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--Modal for livestream ends-->
+
+	<!-- Modal - For Login-->
+	<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+	  <div class="modal-dialog" role="document">
+	    <div class="modal-content">
+	      <div class="modal-header">
+	        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+	        <h4 class="modal-title" id="myModalLabel">Login to OpenLearn (Instructor)</h4>
+	      </div>
+
+				<div class="modal-body">
+					<form action="index.php" method="get">
+						<div class="form-group">
+								<label for="InputEmail1">Email address</label>
+								<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" required>
+							</div>
+
+							<div class="form-group">
+								<label for="exampleInputPassword1">Password</label>
+								<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" required>
+							</div>
+
+							<div class="form-check">
+								<label class="form-check-label">
+									<input type="checkbox" class="form-check-input">&nbsp;Remember me
+								</label>
+							</div>
+	      </div>
+
+				<div class="modal-footer">
+	        	<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+	        	<input type="submit" value="Login" class="btn btn-primary">
+				</form>
+	      </div>
+	    </div>
+	  </div>
+	</div>
+	<!--Modal ends-->
+
 <?php
 
     require 'config.php'; //For database connectivity
@@ -36,28 +228,51 @@
     $directory = $directory.$file_name.rand().'.'.$file_extension; //E.g. - /var/www/html/open-learning/profile_pictures/filename.png
     /* Part 3.2 ends */
 
-    /* Part 3.3: Checking if the website is valid. */
-    $website = filter_var($website, FILTER_SANITIZE_URL); // Remove all illegal characters from a URL
-    if (!filter_var($website, FILTER_VALIDATE_URL)) {
-        echo("$url is not a valid URL");
+    /* Part 3.3: Checking if the website and Twitter URLs are valid. */
+    if (!empty($website)) {
+        if (!filter_var($website, FILTER_VALIDATE_URL)) {
+            echo "
+                <div class='container'>
+                    <div class='row'>
+                        <div class='col-md-12 animate-box'>
+                            <p class='text-center'> - The URL must be in the format http(s)://www.domain.com.</p></div></div></div>";
+        }
     }
-    
+        
+    if (!empty($twitter)) {
+        if (!filter_var($twitter, FILTER_VALIDATE_URL, FILTER_FLAG_PATH_REQUIRED)) {
+            echo "<div class='container'>
+            <div class='row'>
+            <div class='col-md-12 animate-box'>
+                    <p class='text-center'>The Twitter URL must be in the format https://twitter.com/YourUsername</p></div></div></div>";
+        }
+    }
+    /*Part 3.3 ends */
 
 
+    /*Part 4: Moving the data into the MySQL database and the profile picture into the disk */    
+    move_uploaded_file($_FILES['picture']['tmp_name'], $directory);
 
+    $query = "INSERT INTO instructor VALUES ('$first_name', '$last_name', '$email', '$password', '$website', '$twitter', '$picture', '$about')";
+    $result = mysqli_query($connection, $query);
 
+    if($result){
+        echo "
+        <div class='container'>
+            <div class='row'>
+                <div class='col-md-12 animate-box'>
+                    <h3 class='text-center'>Congratulations $first_name for being a part of our family now!</h3>
+                    <button class='btn btn-primary'><a href='../login.php'>Login now</a></button>     
+                </div>
+            </div>
+        </div>";
+    }
+    else {
+        echo "<p>Error occurred: ".mysqli_error();
+    }
 
-    if (!move_uploaded_file($_FILES['picture']['tmp_name'], $directory))
-
-
+    mysqli_close($connection);
 ?>
-
-
-
-
-
-
-<!--Sign up Code ends -->
 
 <div id="fh5co-register" style="background-image: url(images/studying.jpg);">
 		<div class="overlay"></div>
