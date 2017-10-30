22c84f31401e8083d729ae4d5c5ca2b34d6112f8
Subhadeep Dey <contact.sdey@gmail.com>
2017-10-31 01:49:12 +0530

    Modifications

--- PHP/register.php
+++ PHP/register.php
@@ -27,30 +27,30 @@
 	<link href="https://fonts.googleapis.com/css?family=Roboto+Slab:300,400" rel="stylesheet">
 
 	<!-- Animate.css -->
-	<link rel="stylesheet" href="css/animate.css">
+	<link rel="stylesheet" href="../css/animate.css">
 	<!-- Icomoon Icon Fonts-->
-	<link rel="stylesheet" href="css/icomoon.css">
+	<link rel="stylesheet" href="../css/icomoon.css">
 	<!-- Bootstrap  -->
-	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="../css/bootstrap.css">
 
 	<!-- Magnific Popup -->
-	<link rel="stylesheet" href="css/magnific-popup.css">
+	<link rel="stylesheet" href="../css/magnific-popup.css">
 
 	<!-- Owl Carousel  -->
-	<link rel="stylesheet" href="css/owl.carousel.min.css">
-	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+	<link rel="stylesheet" href="../css/owl.carousel.min.css">
+	<link rel="stylesheet" href="../css/owl.theme.default.min.css">
 
 	<!-- Flexslider  -->
-	<link rel="stylesheet" href="css/flexslider.css">
+	<link rel="stylesheet" href="../css/flexslider.css">
 
 	<!-- Pricing -->
-	<link rel="stylesheet" href="css/pricing.css">
+	<link rel="stylesheet" href="../css/pricing.css">
 
 	<!-- Theme style  -->
-	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="../css/style.css">
 
 	<!-- Modernizr JS -->
-	<script src="js/modernizr-2.6.2.min.js"></script>
+	<script src="../js/modernizr-2.6.2.min.js"></script>
 	<!-- FOR IE9 below -->
 	<!--[if lt IE 9]>
 	<script src="js/respond.min.js"></script>
@@ -109,12 +109,12 @@
 				</div>
 				<div class="col-xs-10 text-right menu-1">
 					<ul>
-						<li><a href="index.php">Home</a></li>
-						<li><a href="courses.php">Courses</a></li>
-						<li><a href="instructors.php">Instructors</a></li>
+						<li><a href="../index.php">Home</a></li>
+						<li><a href="../courses.php">Courses</a></li>
+						<li><a href="../instructors.php">Instructors</a></li>
 						<li><a href="#"  data-toggle="modal" data-target="#livestream">Livestream</a></li>
-						<li><a href="about.php">About</a></li>
-						<li><a href="contact.php">Contact</a></li>
+						<li><a href="../about.php">About</a></li>
+						<li><a href="../contact.php">Contact</a></li>
 						<li class="btn-cta" data-toggle="modal" data-target="#myModal"><a href="#"><span>Login</span></a></li>
 						<li class="btn-cta"><a href="signup.php"><span>Become an Instructor</span></a></li>
 					</ul>
@@ -200,14 +200,14 @@
     //-----------------------
 
     /* Part 1: Preventing SQL Injection */
-    $first_name = mysqli_real_escape_string($_POST['first_name']);
-    $last_name = mysqli_real_escape_string($_POST['last_name']);
-    $email = mysqli_real_escape_string($_POST['email']);
-    $password = mysqli_real_escape_string($_POST['password']);
-    $website = mysqli_real_escape_string($_POST['website']);
-    $twitter = mysqli_real_escape_string($_POST['twitter']);
-    $picture = mysqli_real_escape_string($_FILES['picture']['name']);
-    $about = mysqli_real_escape_string($_POST['about']);
+    $first_name = mysqli_real_escape_string($connection, $_POST['first_name']);
+    $last_name = mysqli_real_escape_string($connection, $_POST['last_name']);
+    $email = mysqli_real_escape_string($connection, $_POST['email']);
+    $password = mysqli_real_escape_string($connection, $_POST['password']);
+    $website = mysqli_real_escape_string($connection, $_POST['website']);
+    $twitter = mysqli_real_escape_string($connection, $_POST['twitter']);
+    $picture = mysqli_real_escape_string($connection, $_FILES['picture']['name']);
+    $about = mysqli_real_escape_string($connection, $_POST['about']);
     /* Part 1 ends */
 
 
@@ -219,8 +219,8 @@
     /* Part 3: Storing the profile picture in disk and doing validation */
     /* PART 3.1: Extracting file extensions and filename for renaming the file later on. */
     $file_properties = pathinfo($picture);
-    $file_extension = $file_properties['extension'];
     $file_name = $file_properties['filename'];
+    $file_extension = $file_properties['extension'];
     /* PART 3.1 ends */
 
     /* Part 3.2: Preparing file rename */
@@ -271,7 +271,7 @@
     mysqli_close($connection);
 ?>
 
-<div id="fh5co-register" style="background-image: url(images/studying.jpg);">
+<div id="fh5co-register" style="background-image: url(../images/studying.jpg);">
 		<div class="overlay"></div>
 		<div class="row">
 			<div class="col-md-8 col-md-offset-2 animate-box">
@@ -286,13 +286,13 @@
 		</div>
 	</div>
 
-	<footer id="fh5co-footer" role="contentinfo" style="background-image: url(images/mountain.jpg);">
+	<footer id="fh5co-footer" role="contentinfo" style="background-image: url(../images/mountain.jpg);">
 		<div class="overlay"></div>
 		<div class="container">
 			<div class="row row-pb-md">
 				<div class="col-md-3 fh5co-widget">
-					<h3>About Education</h3>
-					<p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit. Eos cumque dicta adipisci architecto culpa amet.</p>
+                <h3>About OpenLearn</h3>
+                <p>OpenLearn is a global marketplace for learning and teaching online where students are mastering new skills and achieving their goals by learning from an extensive library of over 55,000 courses taught by expert instructors.</p>     
 				</div>
 				<div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1 fh5co-widget">
 					<h3>Learning</h3>
@@ -337,15 +337,14 @@
 					</ul>
 				</div>
 			</div>
-
-			<div class="row copyright">
-				<div class="col-md-12 text-center">
-					<p>
-						<small class="block">&copy; 2016 Free HTML5. All Rights Reserved.</small>
-						<small class="block">Designed by <a href="http://freehtml5.co/" target="_blank">FreeHTML5.co</a> Demo Images: <a href="http://unsplash.co/" target="_blank">Unsplash</a> &amp; <a href="https://www.pexels.com/" target="_blank">Pexels</a></small>
-					</p>
-				</div>
-			</div>
+            <div class="row copyright">
+            <div class="col-md-12 text-center">
+                <p>
+                    <small class="block">&copy; 2017 OpenLearn, Inc.&nbsp;&nbsp;All Rights Reserved.</small>
+                    <small class="block">Designed by <a href="https://www.sddey.com" target="_blank">Subhadeep Dey</a>.</small>
+                </p>
+            </div>
+        </div>
 
 		</div>
 	</footer>
@@ -356,31 +355,31 @@
 	</div>
 
 	<!-- jQuery -->
-	<script src="js/jquery.min.js"></script>
+	<script src="../js/jquery.min.js"></script>
 	<!-- jQuery Easing -->
-	<script src="js/jquery.easing.1.3.js"></script>
+	<script src="../js/jquery.easing.1.3.js"></script>
 	<!-- Bootstrap -->
-	<script src="js/bootstrap.min.js"></script>
+	<script src="../js/bootstrap.min.js"></script>
 	<!-- Waypoints -->
-	<script src="js/jquery.waypoints.min.js"></script>
+	<script src="../js/jquery.waypoints.min.js"></script>
 	<!-- Stellar Parallax -->
-	<script src="js/jquery.stellar.min.js"></script>
+	<script src="../js/jquery.stellar.min.js"></script>
 	<!-- Carousel -->
-	<script src="js/owl.carousel.min.js"></script>
+	<script src="../js/owl.carousel.min.js"></script>
 	<!-- Flexslider -->
-	<script src="js/jquery.flexslider-min.js"></script>
+	<script src="../js/jquery.flexslider-min.js"></script>
 	<!-- countTo -->
-	<script src="js/jquery.countTo.js"></script>
+	<script src="../js/jquery.countTo.js"></script>
 	<!-- Magnific Popup -->
-	<script src="js/jquery.magnific-popup.min.js"></script>
-	<script src="js/magnific-popup-options.js"></script>
+	<script src="../js/jquery.magnific-popup.min.js"></script>
+	<script src="../js/magnific-popup-options.js"></script>
 	<!-- Google Map -->
 	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCefOgb1ZWqYtj7raVSmN4PL2WkTrc-KyA&sensor=false"></script>
-	<script src="js/google_map.js"></script>
+	<script src="../js/google_map.js"></script>
 	<!-- Count Down -->
-	<script src="js/simplyCountdown.js"></script>
+	<script src="../js/simplyCountdown.js"></script>
 	<!-- Main -->
-	<script src="js/main.js"></script>
+	<script src="../js/main.js"></script>
 	<script>
     var d = new Date(new Date().getTime() + 1000 * 120 * 120 * 2000);
 
