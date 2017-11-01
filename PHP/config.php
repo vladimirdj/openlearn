ef55dd44672cc9b8fd4f0408a71aa90349a43a50
Subhadeep Dey <contact.sdey@gmail.com>
2017-11-02 01:40:44 +0530

    Bug fixes

--- PHP/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-   
-    $connection = mysqli_connect('localhost', 'root', 'toor', 'openlearn');
-
-    if(!$connection)
-    {
-        die ("Error " . mysqli_connect_errno() . ": " . mysqli_connect_error(). $PHP_EOL);
-    }
-?>
