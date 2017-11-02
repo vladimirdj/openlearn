3871ee97c77d32a0a264769b638a385f0f201a85
Elian Doran <elinnum@gmail.com>
2017-11-02 03:29:39 +0200

    Remove whitespace

--- ajax-signup.php
+++ ajax-signup.php
@@ -3,15 +3,15 @@
 	header('Content-type: application/json');
 
 	require_once 'config.php';
-	
+
 	$response = array();
 
 	if ($_POST) {
-		
+
 		$name = trim($_POST['name']);
 		$email = trim($_POST['email']);
 		$password = trim($_POST['password']);
-		
+
 		$full_name = strip_tags($name);
 		$user_email = strip_tags($email);
 		$user_pass = strip_tags($password);
@@ -19,7 +19,7 @@
 		$twitter = $_POST['twitter'];
 		//$user_picture = $_FILES['profile_picture']['name'];
 		$about = strip_tags($_POST['about']);
-		
+
 		//Password hashing
 		$hashed_password = password_hash($user_pass, PASSWORD_DEFAULT);
 
@@ -32,25 +32,25 @@
         $directory = "/var/www/html/open-learning/profile_pictures/";
 		$directory .= $pic_filename.rand().'.'.$pic_file_extension;
 
-		move_uploaded_file($_FILES['picture']['tmp_name'], $directory); //Moving file to the disk	
+		move_uploaded_file($_FILES['picture']['tmp_name'], $directory); //Moving file to the disk
 
 
-		/* Test upload 
+		/* Test upload
 		if(isset($_FILES['profile_picture']))
 		{
 			$img = $_FILES['profile_picture']['name'];
 			$tmp = $_FILES['profile_picture']['tmp_name'];
 			$path = 'profile_pictures/';
-				
+
 			// get uploaded file's extension
 			$ext = strtolower(pathinfo($img, PATHINFO_EXTENSION));
-			
+
 			// can upload same image using rand function
 			$final_image = rand(1000,1000000).$img;
-			
+
 			// check's valid format
-			$path = $path.strtolower($final_image);	
-					
+			$path = $path.strtolower($final_image);
+
 			move_uploaded_file($tmp, $path);
 		}
 		/* Test upload ends */
@@ -58,7 +58,7 @@
 		$query = "INSERT INTO instructor VALUES ('$full_name', '$user_email', '$hashed_password', '$website', '$twitter', '$directory', '$about')";
 
 		$stmt =  mysqli_query($link, $query);
-		
+
 		// check for successful registration
         if ($stmt) {
 			$response['status'] = 'success';
@@ -67,8 +67,8 @@
             $response['status'] = 'error'; // could not register
 			$response['message'] = '<span class="glyphicon glyphicon-info-sign"></span> &nbsp; Could not register now. Please try again later.';
 			echo $_FILES['profile_pic'];
-        }	
+        }
 	}
-	
+
 	echo json_encode($response);
 ?>
