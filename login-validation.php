68e143323af0f574ecbe2db840499d0108af9c75
Subhadeep Dey <contact.sdey@gmail.com>
2017-11-05 00:58:24 +0530

    Bug fixes.

--- login-validation.php
+++ login-validation.php
@@ -11,23 +11,20 @@
     $inst_email = mysqli_real_escape_string($link, $_POST['instEmail']);
     $inst_password = mysqli_real_escape_string($link, $_POST['instPassword']); 
 
-    $query = "SELECT `password` FROM `instructor` WHERE (`email`='$inst_email')";
+    $query = "SELECT `id`, `password` FROM `instructor` WHERE (`email`='$inst_email')";
     $execute_query = mysqli_query($link, $query);
     $get_row = mysqli_fetch_assoc($execute_query);
+
+    $inst_id = $get_row['id'];
     $hashed_db_password = $get_row['password'];
 
     /*Comparing the entered password with the one in the database, and returning a boolean value.*/
     $isCorrectPassword = password_verify($inst_password, $hashed_db_password);
 
-
-    $query_login = "SELECT `id`, `email`, `password` FROM `instructor` WHERE `email`='$inst_email' AND `password`='$hashed_db_password'";
-    $stmt =  mysqli_query($link, $query_login);
-    $count = mysqli_num_rows($stmt);
-
-	// check for successful registration
-    if (($stmt===true) && ($isCorrectPassword===true) && ($count===1)) {
+	// check for successful validation
+    if (($execute_query) && ($isCorrectPassword)) {
 		$response['status'] = 'success';
-		$response['message'] = '<span class="glyphicon glyphicon-ok"></span> &nbsp;Your message has been sent successfully!';
+		$response['message'] = '<span class="glyphicon glyphicon-ok"></span> &nbsp;Details verified!';
     } else {
         $response['status'] = 'error'; // could not log in
         $response['message'] = '<span class="glyphicon glyphicon-info-sign"></span> &nbsp;Email or password is incorrect. Please try again.';
