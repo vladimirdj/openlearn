fe0019871b45f3065af37b8bf7db8a080a99a4f1
Subhadeep Dey <contact.sdey@gmail.com>
2017-11-09 02:49:32 +0530

    Bug fix.

--- view-course.php
+++ view-course.php
@@ -5,7 +5,7 @@
 	<link rel="shortcut icon" href="favicon.png" />
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Manage Course &mdash; Free Online Courses at OpenLearn!</title>
+	<title>View Course &mdash; Free Online Courses at OpenLearn!</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="Free HTML5 Website Template by freehtml5.co" />
 	<meta name="keywords" content="free website templates, free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
