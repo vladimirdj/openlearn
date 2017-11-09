e109480a0a8767dd07b5f7810c8fbdeb047ef09a
Subhadeep Dey <contact.sdey@gmail.com>
2017-11-10 00:03:52 +0530

    Bug fixes.

--- view-course.php
+++ view-course.php
@@ -12,7 +12,7 @@
 	<meta name="author" content="freehtml5.co" />
 
 	<?php
-	
+
 		ini_set ('log_errors', 'on'); //Logging errors
 
 		session_start();
@@ -34,13 +34,20 @@
 			//Getting instructor's first name (accessible as zeroth index)
 			$get_name = explode(' ',trim($inst_name));
             $inst_first_name = $get_name[0];
-            
+
             //Getting course information
             $getCourseInfo = "SELECT * from `courses` where `course_id`='{$_GET['course_id']}'";
 			$queryCourse = mysqli_query($link, $getCourseInfo);
             $rowCourse = mysqli_fetch_assoc($queryCourse);
 
-		} 
+		} else {
+			//Redirect the instructor to login page if he/she is not logged in.
+			echo "
+				<script type='text/javascript'>
+					window.location.href = 'login.php';
+				</script>
+			";
+        }
 
 	?>
 
@@ -136,9 +143,9 @@
 							<li><a href="courses.php">Courses</a></li>
 							<li><a href="instructors.php">Instructors</a></li>
 							<li><a href="about.php">About</a></li>
-							<li><a href="contact.php">Contact</a></li> 
+							<li><a href="contact.php">Contact</a></li>
 							&nbsp;&nbsp;&nbsp;
-							
+
 							<?php
 								if(isset($_SESSION['inst_id'])) {
 									echo "
@@ -146,14 +153,10 @@
 								<ul class='dropdown'>
 									<li><a href='profile.php?inst_id=$inst_id'><i class='fa fa-user'></i>&nbsp;&nbsp;Profile</a></li>
 									<li><a href='http://localhost/open-learning/admin/admin_dashboard.php'><i class='fa fa-tachometer'></i>&nbsp;&nbsp;Dashboard</a></li>
-									<li><a href='#'><i class='fa fa-question-circle'></i>&nbsp;&nbsp;Help &amp; Support</a></li>									
+									<li><a href='#'><i class='fa fa-question-circle'></i>&nbsp;&nbsp;Help &amp; Support</a></li>
 									<li><a href='#' data-toggle='modal' data-target='#logoutModal'><i class='fa fa-sign-out'></i>&nbsp;&nbsp;Logout</a></li>
 								</ul>
 							</li>";
-							} else {
-								echo "<li class='btn-cta' data-toggle='modal' data-target='#myModal'><a href='#'><span>Login</span></a></li>
-								
-								<li class='btn-cta'><a href='signup.php'><span>Become an Instructor</span></a></li>";
 							}
 						?>
 
@@ -164,47 +167,6 @@
 			</div>
 		</div>
 	</nav>
-
-	<!-- Modal - For Login-->
-	<div class="modal fade" id="myModal" tabindex="-1" autocomplete="off" role="dialog" aria-labelledby="myModalLabel">
-		<div class="modal-dialog" role="document">
-			<div class="modal-content">
-				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-					<h4 class="modal-title" id="myModalLabel">Login to OpenLearn&nbsp;(For Instructors Only)</h4>
-				</div>
-
-				<div class="modal-body">
-					<form id="login-form" method="POST" autocomplete="off">
-						<div class="form-group">
-								<b>Email address</b>
-								<input type="email" name="instEmail" class="form-control" id="instEmail" placeholder="Enter email">
-								<span class="help-block" id="error"></span>
-							</div>
-
-							<div class="form-group">
-								<b>Password</b>
-								<input type="password" name="instPassword" class="form-control" id="instPassword" placeholder="Password">
-								<span class="help-block" id="error"></span>
-							</div>
-
-							<div class="form-check">
-								<label class="form-check-label">
-									<input type="checkbox" id="rememberMe"class="form-check-input">&nbsp;Remember me
-								</label>
-							</div>
-							<div id="errorDiv"></div> 
-				</div>
-
-				<div class="modal-footer">
-						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-						<button type="submit" id="btn-login" class="btn btn-primary">Login</button>
-				</form>
-				</div>
-			</div>
-		</div>
-	</div>
-	<!--Modal for login ends-->
 
 	<!-- Modal - For Logout-->
 	<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
@@ -234,17 +196,17 @@
 <br><br><br>
 
 	<!--Experiment About Course Part -->
-    <div class="container" style="width:900px;">  
+    <div class="container" style="width:900px;">
 	<h1 align="center"><i class="fa fa-graduation-cap fa-3x"></i><br></h3>
-   <h3 align="center"><?php echo $rowCourse['course_name'];?></h3>  
+   <h3 align="center"><?php echo $rowCourse['course_name'];?></h3>
    <p align="center"><?php echo $rowCourse['course_info'] ?></p>
-   <br />  
+   <br />
    <div class="table-responsive">
     <div id="employee_table">
      <table class="table table-hover table-bordered">
       <thead>
 		  <tr>
-       		<th width="70%">Title</th>  
+       		<th width="70%">Title</th>
        		<th width="4%">View</th>
 		  </tr>
 	</thead>
@@ -258,10 +220,10 @@
         while($row_course_content = mysqli_fetch_assoc($queryCourseContent))
         {
     ?>
-    
+
       <tr>
        <td><?php echo $row_course_content["video_title"]; ?></td>
-       <td><button class="btn btn-primary" data-toggle="modal" data-target="#add_data_Modal">View</button></td>
+       <td><button class="btn btn-primary view-button" data-toggle="modal" data-target="#add_data_Modal" data-video-id="<?= $row_course_content["video_link"] ?>">View</button></td>
       </tr>
       <?php
       }
@@ -269,10 +231,10 @@
 	  </tbody>
      </table>
     </div>
-   </div>  
+   </div>
   </div>
- </body>  
-</html>  
+ </body>
+</html>
 
 <div id="add_data_Modal" class="modal fade">
  <div class="modal-dialog">
@@ -283,8 +245,8 @@
    </div>
    <div class="modal-body" id="yt-player">
 	<!-- Form ends -->
-	<iframe width="560" height="315" src="https://www.youtube.com/embed/4axuf8Qvotw" frameborder="0" allowfullscreen></iframe>
-    
+	<iframe width="560" height="315" src="" id="video" frameborder="0" allowfullscreen></iframe>
+
    </div>
    <div class="modal-footer">
     <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
@@ -294,7 +256,7 @@
  </div>
 </div>
 	<!--Experiment ends -->
-	
+
 	<script type="text/javascript">
     $('#add_data_Modal').on('hidden.bs.modal', function () {
         callPlayer('yt-player', 'stopVideo');
@@ -374,20 +336,12 @@
 
     <!-- jQuery -->
 	<script src="js/jquery.min.js"></script>
-	<script src="assets/jquery.validate.min.js"></script>
-	<script src="js/additional-methods.js"></script>
-	<script src="js/extension.js"></script> <!--Message is validated and sent-->
-	<script src="login.js"></script>
-	<script src="assets/jquery.validate.min.js"></script>
-	<script src="js/additional-methods.js"></script>
-	<script src="js/extension.js"></script> <!--Message is validated and sent-->
-	<script src="login.js"></script>
 	<!-- jQuery Easing -->
 	<script src="js/jquery.easing.1.3.js"></script>
 	<!-- Bootstrap -->
 	<script src="js/bootstrap.min.js"></script>
-    
-    
+
+
 	<!-- Waypoints -->
 	<script src="js/jquery.waypoints.min.js"></script>
 	<!-- Stellar Parallax -->
@@ -422,6 +376,12 @@
         day: d.getDate(),
         enableUtc: false
     });
+
+	$(".view-button").click(function() {
+		var video_url = $(this).attr("data-video-id");
+		video_url = "https://youtube.com/embed/" + video_url;
+		$("#video").attr("src", video_url);
+	});
 	</script>
 	</body>
 </html>
