6c3279809d2642fc89f3e883d6dcd4876374c569
Subhadeep Dey <contact.sdey@gmail.com>
2017-11-10 13:17:59 +0530

    Bug fixes.

--- view-course.php
+++ view-course.php
@@ -129,7 +129,7 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-xs-2">
-						<div id="fh5co-logo"><a href="index.php"><i class="icon-study"></i><span>&nbsp;Open</span><font color="#2D6CDF">Learn&nbsp;</font><font color="red">/Admin</font></a></div>
+						<div id="fh5co-logo"><a href="index.php"><i class="icon-study"></i><span>&nbsp;Open</span><font color="#2D6CDF">Learn&nbsp;</font></a></div>
 					</div>
 					<div class="col-xs-10 text-right menu-1">
 						<ul>
